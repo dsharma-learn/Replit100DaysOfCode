2e1a4f521339880c46178ac17915c75b80e09178
Deepak Sharma <deepusharma@users.noreply.github.com>
2022-09-24 09:13:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+print("Hello Replit")
+
+print("""
+Type 
+  in 
+    whatever
+        format  """)
+
+
+print("= MUSIC+ =")
+print("> Songs")
+print("> Albums")
+print("> Artists")
+
+
+print("""
+Deepak Sharma
+September 24th 2022 
+
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+
+I am Feeling 😇 ?? 
+
+You can follow my progress at replit.com/@DeepakSharma92
+""")
